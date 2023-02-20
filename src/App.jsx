e89186bf78193c66610f26bd594884107d896aa3
unknown <jmarcanoy@gmail.com>
2023-02-19 20:04:02 -0400

    comienzo de web con react peliculas

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+import YouTube from 'react-youtube';
+
+
+const App = () => {
+  return (
+    <div>Site movie</div>
+  )
+}
+
+export default App
