977faaf218014a7578b0c6c3c79931f68455507d
unknown <jmarcanoy@gmail.com>
2023-02-20 21:12:18 -0400

    copie lo que esta en el repositorio de github

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,131 @@
-import React, {useState, useEffect} from 'react'
+
+import { useEffect } from 'react';
 import axios from 'axios'
+import './App.css';
+import { useState } from 'react';
 import YouTube from 'react-youtube';
 
+function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "19ca1550a775dc7d02c3961aeedc1589";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 
-const App = () => {
-  const API_URL="https://api.themoviedb.org/3/"
-  const KEY="19ca1550a775dc7d02c3961aeedc1589"
-  const IMAGE_PATH="https://image.tmdb.org/t/p/original"
-  const IMAGE_URL="https://image.tmdb.org/t/p/original"
+  // endpoint para las imagenes
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
-  //variables de estado
+  // variables de estado
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  //const [selectedMovie, setSelectedMovie] = useState({})
+  const [trailer, setTrailer] = useState(null);
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  const [playing, setPlaying] = useState(false);
 
-  const [movies, setMovies] = useState([])
-  const [searchkey, setSearchkey] = useState("")
-  const [trailer, setTrailer] = useState(null)
-  const [movie, setMovie] = useState({title: "Cargando peliculas"})
-  const [playing, setPlaying] = useState(false)
-
-
-  //realizar la peticion get
-
-  const fetchMovies = async(searchkey) => {
-    const type = searchkey ? "search" : "discover"
-    const {data : {results}, } = await axios.get(`${API_URL}/${type}/movie`, {
-      params : {
-        api_key : KEY,
-        query: searchkey,
-      }
-    }) 
-    setMovies(results)
-    setMovie(results[0])
-    if(results.length) {
-      await fetchmovie(results[0].id)
-    }
-  }
-
-  //funcion para la reproduccion de video si se selecciona una pelicula
-
-  const fetchmovie = async(id) => {
-    const {data} = await axios.get(`${API_URL}/movie/${id}`, {
+  // funcion para realizar la peticion get a la api
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
-        api_key: KEY,
-        append_to_respond: "videos"
-      }
-    })
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+    //console.log('data',results);
+    //setSelectedMovie(results[0])
 
-    //estan sentecia de abajo es para no tener resultados con error al momento de realizar la busqueda
-    if(data.videos && data.videos.results) {
-      const trailer = daa.video.results(find(
-        (vid) = vid.name === "Official Trailer"
-      ))
-      setTrailer(trailer ? trailer : data.videos.results[0]) 
+    setMovies(results);
+    setMovie(results[0]);
+
+    if (results.length) {
+      await fetchMovie(results[0].id);
     }
-    setMovie(data)
-  }
-  const selectMovie = async(movie) =>{
-    fetchmovie(movie.id)
-    setMovie(movie)
-    window.scrollTo(0,0)
-  }
+  };
 
+  // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
 
-  //implementando la funcion buscar 
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+    //return data
+    setMovie(data);
+  };
+
+  const selectMovie = async (movie) => {
+    // const data = await fetchMovie(movie.id)
+    // console.log(data);
+    // setSelectedMovie(movie)
+    fetchMovie(movie.id);
+
+    setMovie(movie);
+    window.scrollTo(0, 0);
+  };
+
+  // funcion para buscar peliculas
   const searchMovies = (e) => {
-    e.preventDefault()
-    fetchMovies(searchkey)
-
-
-  }
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
-  
+    fetchMovies();
+  }, []);
 
   return (
-    
-    /*contenedor que va a mostrar el poster de las peliculas actuales*/
-    <>
+    <div>
+      
+
+      {/* contenedor para previsualizar  */}
+      {/* <div>
+        <div
+          className="viewtrailer"
+          style={{
+            backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+          }}
+        >
+          
+          
+
+          <div className="container">
+
+            
+            
+            <button className="boton">Play Trailer</button>
+            <h1 className="text-white">{movie.title}</h1>
+            {movie.overview ? (
+              <p className="text-white">{movie.overview}</p>
+            ) : null}
+          </div>
+        </div>
+      </div> */}
+
+      {/* esto es por prueba */}
       <div>
         <main>
           {movie ? (
-            <div className='viewtrailer' style={{backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,}}>
-                {playing ? (
+            <div
+              className="viewtrailer"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
                 <>
-                  <YouTube videoId={trailer.key} className="Reproductor container" containerClassName={"youtube-container"}
+                  <YouTube
+                    videoId={trailer.key}
+                    className="reproductor container"
+                    containerClassName={"youtube-container amru"}
                     opts={{
                       width: "100%",
                       height: "100%",
@@ -93,62 +134,79 @@ const App = () => {
                         controls: 0,
                         cc_load_policy: 0,
                         fs: 0,
-                        tv_load_policy: 0,
+                        iv_load_policy: 0,
                         modestbranding: 0,
                         rel: 0,
                         showinfo: 0,
                       },
                     }}
                   />
-                  <button onClick={() =>setPlaying(false)} className="boton">Cerrar</button>
+                  <button onClick={() => setPlaying(false)} className="boton">
+                    Close
+                  </button>
                 </>
-            ): (
-              <div className="container">
-                <div className="">
-                  {trailer ? (
-                    <button className='boton' onClick={() =>setPlaying(true)} type="button">Play </button>
-                  ) : (
-                    <h5 className='text-white'>Disculpe, no existe este trailer</h5>
-                  )}
-                  <h1 className='text-white'>{movie.title}</h1>
-                  <p className='text-white'>{movie.overview}</p>
+              ) : (
+                <div className="container">
+                  <div className="">
+                    {trailer ? (
+                      <button
+                        className="boton"
+                        onClick={() => setPlaying(true)}
+                        type="button"
+                      >
+                        Play Trailer
+                      </button>
+                    ) : (
+                      "Sorry, no trailer available"
+                    )}
+                    <h1 className="text-white">{movie.title}</h1>
+                    <p className="text-white">{movie.overview}</p>
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
             </div>
-            ) : null}
-          
+          ) : null}
         </main>
       </div>
-    
-      <div className="container mt-3">
-        <h2 className='text-center mt-3 mb-5'>Trailers de Peliculas</h2>
-        {/*Buscador*/}
-          <div className="row ">
-            <div className="col-md-4 d-flex">
-              <form className='container mb-4 d-flex' onSubmit={searchMovies}>
-                <input type="text" placeholder='Busqueda' className='form-control' onChange={(e)=>setSearchkey(e.target.value)} />
-                <button className='btn btn-primary'>Buscar</button>
-              </form>
-            </div>
-          </div>
-      {/*aqui va todo el contenedor del banner y video*/}
 
-        <div className="row">
-          {
-            movies.map((movie) => (
-              <div key={movie.id} className="col-md-3 mb-3" onClick={() =>selectMovie()}>
-                <img src={`${IMAGE_URL + movie.poster_path}`} height={450} width="100%" />
-                <h4 className='text-center'>{movie.title}</h4>
-              </div>
-            ))
-          }
+      <h2 className="text-center mt-5 mb-5">Trailer Popular Movies</h2>
+
+      {/* el buscador */}
+      <form className="container mb-4" onSubmit={searchMovies}>
+        <div className="col-md-4 d-flex">
+
+        <input
+          type="text"
+          placeholder="Buscar"
+          onChange={(e) => setSearchKey(e.target.value)}
+          className="form-control col-4"
+        />
+        <button className="btn btn-primary">Buscar</button>
         </div>
+      </form>
 
+      {/* contenedor para mostrar los posters y las peliculas en la peticion a la api */}
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
       </div>
-    
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
